a709b44bfd0b349a3a5ccc3d5a1cdefd13137342
joan9340 <joanmunoz50@gmail.com>
2025-10-14 19:13:46 -0500

    proyecto final terminado

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+//BrowserRouter para las rutas
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-import 'animate.css';
-
+import "animate.css";
 
 // ReactDOM.createRoot() es el nuevo método para renderizar en React 18+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {/* BrowserRouter maneja la navegación entre rutas */}
+    <BrowserRouter basename="/ApiSimpsons_2025IIg1_Munoz_Joan">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   base: "/ApiSimpsons_2025IIg1_Munoz_Joan/",
   plugins: [react()],
-})
+});
